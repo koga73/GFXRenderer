f38efc3ee27bf35ebc5db887dba54a0e2f996cae
AJ Savino <aj.savino@moxieusa.com>
2016-01-12 14:37:53 -0500

    added github url to file comments

--- js/GFXRenderer.js
+++ js/GFXRenderer.js
@@ -1,5 +1,6 @@
 /*
-* GFXRenderer v1.0.4 Copyright (c) 2015 AJ Savino
+* GFXRenderer v1.0.4 Copyright (c) 2016 AJ Savino
+* https://github.com/koga73/GFXRenderer/
 * 
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
